081798f3505500af078dcaa62888b76557264c9b
Roy Ge <rgecoder@gmail.com>
2020-01-07 17:41:53 -0800

    Add rgbGame.css extra styling

--- rgbGame.js
+++ rgbGame.js
@@ -62,7 +62,7 @@ resetButton.addEventListener("click", function() {
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
   }
-  h1.style.backgroundColor = "#232323";
+  h1.style.backgroundColor = "steelBlue";
 });
 
 colorDisplay.textContent = pickedColor;
@@ -82,7 +82,7 @@ for (var i = 0; i < squares.length; i++) {
       changeColors(clickedColor);
       h1.style.backgroundColor = clickedColor;
     } else {
-      this.style.backgroundColor = "#232323";
+      this.style.backgroundColor = "steelBlue";
       messageDisplay.textContent = "Try Again.";
     }
   });
